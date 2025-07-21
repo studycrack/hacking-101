c0b6e153da32362d72e778a47c4e7cfa5f100dd3
studycrack <studycrack@protonmail.com>
2025-07-21 13:42:13 +0900

    Update util.c

--- include/util.c
+++ include/util.c
@@ -3,11 +3,11 @@
 #include <limits.h>
 #include <time.h>
 
-static char guess[] = "我是谁???\0";
+static char guess[] = "Who am I???\0";
 static char filename[] = "image.txt\0";
 
 void get_dir_path(char *path) {
-        // 获取当前可执行文件的绝对路径
+        // Get absolute path of current executable
         int ret = readlink("/proc/self/exe", path, UTIL_MAX_LEN);
         if (ret < 0) {
                 perror("readlink error");
@@ -39,7 +39,7 @@ void print_str_color(char * str, char * color, char * bgcolor)
         int i, padding, remain = 0;
         char upper_str[UTIL_MAX_LEN] = {0};
 
-        // upper string
+        // Convert to uppercase
         for (int i = 0; str[i] != '\0'; i++) {
                 upper_str[i] = toupper(str[i]);
                 if (i == UTIL_MAX_LEN) {
@@ -50,15 +50,15 @@ void print_str_color(char * str, char * color, char * bgcolor)
         padding = (cnt - 2 - strlen(str)) / 2;
         remain = (cnt - 2 - strlen(str)) % 2;
 
-        // padding
+        // Left padding
         for (i = 0; i < padding; i++) {
                 printf("=");
         }
 
-        // print string
+        // Print string
         print_color(upper_str, color, bgcolor);
 
-        // padding
+        // Right padding
         for (i = 0; i < padding; i++) {
                 printf("=");
         }
@@ -72,15 +72,15 @@ static inline void print_time()
 {
 	time_t timep;
 	struct tm *p;
-	time (&timep);
+	time(&timep);
 	p = gmtime(&timep);
-	printf("%d年%d月%d日%d:%d:%d",1900+p->tm_year, 1+p->tm_mon, p->tm_mday, 8+p->tm_hour, p->tm_min, p->tm_sec);
+	printf("%d-%02d-%02d %02d:%02d:%02d", 1900 + p->tm_year, 1 + p->tm_mon, p->tm_mday, 8 + p->tm_hour, p->tm_min, p->tm_sec);
 }
 
 int get_abs_path(char * filename, char *abs_path)
 {
         get_dir_path(abs_path);
-        // get file absolute path
+        // Get full file path
         if (abs_path == NULL) {
                 return -1;
         }
@@ -100,14 +100,16 @@ int print_image(char * pokemon_name, char * filename)
                 return -1;
         }
         
-        // open pokemon text art file
-	if((fptr = fopen(file_path,"r")) == NULL) {
+        // Open Pokémon ASCII art file
+	if((fptr = fopen(file_path, "r")) == NULL) {
 		fprintf(stderr, "error opening %s\n", file_path);
 		return -1;
 	}
 
-        // print pokemon text art
+        // Print title/guess string
         print_str_color(guess, LOG_BLUE, NULL);
+
+        // Print Pokémon art line by line
 	while(fgets(read_string, sizeof(read_string), fptr) != NULL) {
                 if (flag == 0) {
                         flag++;
@@ -120,7 +122,7 @@ int print_image(char * pokemon_name, char * filename)
 	printf("\n");
         sleep(2);
         
-        // print pokemon name
+        // Print Pokémon name
         strcat(pokemon_name, "!");
         print_str_color(pokemon_name, LOG_CYAN, NULL);
         pokemon_name[strlen(pokemon_name) - 1] = '\0';
@@ -132,27 +134,27 @@ int print_image(char * pokemon_name, char * filename)
 void print_desc_name(char * pokemon_name)
 {
         printf("###\n");
-        printf("### 在正式挑战");
+        printf("### Before officially challenging ");
         print_color(pokemon_name, LOG_CYAN, NULL);
-        printf("之前，请仔细阅读并学习描述中的战斗技巧\n");
-	printf("### 如果感到吃力，请先尝试战胜其他低等级的宝可梦\n");
-	printf("### 祝你好运 :)\n");
+        printf(", please carefully read and learn the battle tips in the description.\n");
+	printf("### If it's too difficult, try defeating lower-level Pokémon first.\n");
+	printf("### Good luck :)\n");
 	printf("###\n");
 }
 
 void get_pokemon_name(char * filename, char * name, int num)
 {
-        char fmt[UTIL_MAX_LEN]={0}, file_path[UTIL_MAX_LEN];
+        char fmt[UTIL_MAX_LEN] = {0}, file_path[UTIL_MAX_LEN];
 	FILE * fptr = NULL;
 
-        // open pokemon text art file
+        // Open Pokémon name file
         get_abs_path(filename, file_path);
 	if((fptr = fopen(file_path, "r")) == NULL) {
 		fprintf(stderr, "error opening %s\n", file_path);
                 return;
 	}
         
-        // read until next line
+        // Read up to the nth line
         sprintf(fmt, "%s%d%s", "%", num, "s[^\n]");
         fscanf(fptr, fmt, name);
         fclose(fptr);
@@ -163,15 +165,15 @@ void print_exit()
         int name_len = 32;
         char name[name_len];
 
-        // get pokemon name
+        // Get Pokémon name
         get_pokemon_name(filename, name, name_len);
 
         printf("\n### ");
 	print_time();
-        printf(" 战斗记录:");
+        printf(" Battle log: ");
         print_color(name, LOG_RED, NULL);
-        printf("获胜\n");
-	printf("### 努力提升自己的等级后，再来挑战吧！\n");
+        printf(" won!\n");
+	printf("### Come back and challenge again after leveling up!\n");
         fflush(stdout);
 }
 
@@ -180,13 +182,13 @@ void print_desc()
         int name_len = 32;
         char name[name_len];
 
-        // get pokemon name
+        // Get Pokémon name
         get_pokemon_name(filename, name, name_len);
 
-        // print pokemon text art
+        // Print Pokémon ASCII art
         print_image(name, filename);
 
-        // print pokemon name
+        // Print description message
 	print_desc_name(name);
         fflush(stdout);
 }
